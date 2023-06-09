7186c4d79eb3574b41aee2f28f3ffcc1e76a5c35
Gamnraa <ltaz1466@gmail.com>
2023-06-09 12:50:09 -0400

    Addresses #2

--- modmain.lua
+++ modmain.lua
@@ -1,1 +1,26 @@
 modimport "scripts/axethrowableadditions"
+
+local State = GLOBAL.State
+local FRAMES = GLOBAL.FRAMES
+local TimeEvent = GLOBAL.TimeEvent
+local EventHandler = GLOBAL.EventHandler
+require "stategraphs/commonstates"
+local CommonHandlers = GLOBAL.CommonHandlers
+local STRINGS = GLOBAL.STRINGS
+local TUNING = GLOBAL.TUNING
+local ActionHandler = GLOBAL.ActionHandler
+
+local THROW_AXE = AddAction("THROW_AXE", "Throw Axe", function(act)
+    act.doer.componets.talker:Say("Happy Labor Day!")
+end
+)
+
+local function candoaltattack(inst, doer, target, actions, right)
+    if right and doer.replica.combat and doer.replica.combat:CanTarget(target) and target.replica.combat:CanBeAttacked() then
+        if inst:HasTag("throwableaxe") then
+            table.insert(actions, GLOBAL.ACTIONS.THROW_AXE)
+        end
+    end
+end
+
+AddComponentAction("EQUIPPED", "combatalternativeattack", candoaltattack)
