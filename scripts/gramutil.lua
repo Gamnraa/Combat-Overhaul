8220b94b2e5b78d2210596bc01ad36e6db9f2004
Gamnraa <ltaz1466@gmail.com>
2023-06-08 21:28:46 -0400

    Initial commit

---
+++ scripts/gramutil.lua
@@ -0,0 +1,9 @@
+local State = GLOBAL.State
+local FRAMES = GLOBAL.FRAMES
+local TimeEvent = GLOBAL.TimeEvent
+local EventHandler = GLOBAL.EventHandler
+require "stategraphs/commonstates"
+local CommonHandlers = GLOBAL.CommonHandlers
+local STRINGS = GLOBAL.STRINGS
+local TUNING = GLOBAL.TUNING
+local ActionHandler = GLOBAL.ActionHandler
